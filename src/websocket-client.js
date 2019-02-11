6653e07723e49daad5256bf9764039819154b4ff
Dominic Lee <34794189+dominictwlee@users.noreply.github.com>
2019-02-11 23:01:14 +0000

    prettier format on save

--- src/websocket-client.js
+++ src/websocket-client.js
@@ -1,109 +1,113 @@
-const AWS = require("aws-sdk");
-const db = require("./db");
-
+const AWS = require('aws-sdk');
+const db = require('./db');
 
 class Client {
-    constructor(config) {
-        this.client = undefined;
+  constructor(config) {
+    this.client = undefined;
 
-        if (config) {
-            this._setupClient(config)
-        }
+    if (config) {
+      this._setupClient(config);
+    }
+  }
+
+  // allow just passing a single event to setup the client for ease of use
+  async _setupClient(config) {
+    // fetch config from db if none provided and we do not have a client
+    if (typeof config !== 'object' && !this.client) {
+      const item = await db.Client.get({
+        TableName: db.Table,
+        Key: {
+          [db.Primary.Key]: 'APPLICATION',
+          [db.Primary.Range]: 'WS_CONFIG',
+        },
+      }).promise();
+
+      console.log(`[wsClient][_setupClient][Table Get] Item: \n${JSON.stringify(item, null, 2)}`);
+
+      config = item.Item;
+      config.fromDb = true;
     }
 
-    // allow just passing a single event to setup the client for ease of use
-    async _setupClient(config) {
-        // fetch config from db if none provided and we do not have a client
-        if (typeof config !== 'object' && !this.client) {
-            const item = await db.Client.get({
-                TableName: db.Table,
-                Key: {
-                    [db.Primary.Key]: 'APPLICATION',
-                    [db.Primary.Range]: 'WS_CONFIG'
-                }
-            }).promise();
+    if (!this.client) {
+      if (config.requestContext.apiId) {
+        config.requestContext.domainName = `${config.requestContext.apiId}.execute-api.${
+          process.env.API_REGION
+        }.amazonaws.com`;
+      }
 
-            console.log(`[wsClient][_setupClient][Table Get] Item: \n${JSON.stringify(item, null, 2)}`);
+      this.client = new AWS.ApiGatewayManagementApi({
+        apiVersion: '2018-11-29',
+        endpoint: `https://${config.requestContext.domainName}/${config.requestContext.stage}`,
+      });
 
-            config = item.Item;
-            config.fromDb = true;
-        }
+      // temporarily we update dynamodb with most recent info
+      // after CF support this can go away, we just do this so a single deployment makes this work
+      if (config.fromDb !== true) {
+        await db.Client.put({
+          TableName: db.Table,
+          Item: {
+            [db.Primary.Key]: 'APPLICATION',
+            [db.Primary.Range]: 'WS_CONFIG',
+            requestContext: {
+              domainName: config.requestContext.domainName,
+              stage: config.requestContext.stage,
+            },
+          },
+        }).promise();
+      }
+    }
+  }
 
-        if (!this.client) {
+  async send(connection, payload) {
+    // Cheat and allow event to be passed in
+    // this also lets us default to setupClient too
+    await this._setupClient(connection);
 
-            if (config.requestContext.apiId) {
-                config.requestContext.domainName = `${config.requestContext.apiId}.execute-api.${process.env.API_REGION}.amazonaws.com`
-            }
-
-            this.client = new AWS.ApiGatewayManagementApi({
-                apiVersion: "2018-11-29",
-                endpoint: `https://${config.requestContext.domainName}/${config.requestContext.stage}`
-            });
-
-            // temporarily we update dynamodb with most recent info
-            // after CF support this can go away, we just do this so a single deployment makes this work
-            if (config.fromDb !== true) {
-                await db.Client.put({
-                    TableName: db.Table,
-                    Item: {
-                        [db.Primary.Key]: 'APPLICATION',
-                        [db.Primary.Range]: 'WS_CONFIG',
-                        requestContext: {
-                            domainName: config.requestContext.domainName,
-                            stage: config.requestContext.stage
-                        }
-                    }
-                }).promise();
-            }
-        }
+    let ConnectionId = connection;
+    if (typeof connection === 'object') {
+      ConnectionId = connection.requestContext.connectionId;
     }
 
-    async send(connection, payload) {
-        // Cheat and allow event to be passed in
-        // this also lets us default to setupClient too
-        await this._setupClient(connection);
+    console.log(`[wsClient][send] Connection: ${connection}`);
+    console.log(`[wsClient][send] Payload: \n${JSON.stringify(payload, null, 2)}`);
 
-        let ConnectionId = connection;
-        if (typeof connection === 'object') {
-            ConnectionId = connection.requestContext.connectionId;
+    // client is an instance of AWS.ApiGatewayManagementApi
+    await this.client
+      .postToConnection({ ConnectionId, Data: JSON.stringify(payload) })
+      .promise()
+      .catch(async err => {
+        console.log(JSON.stringify(err));
+
+        if (err.statusCode === 410) {
+          // unsub all channels connection was in
+          const subscriptions = await db.fetchConnectionSubscriptions(ConnectionId);
+
+          console.log(
+            `[wsClient][send][postToConnection] Found stale connection, deleting ${ConnectionId}:`
+          );
+          console.log('[wsClient][send][postToConnection] Unsubscribe from channels:');
+          console.log(JSON.stringify(subscriptions, null, 2));
+
+          const unsubscribes = subscriptions.map(async subscription =>
+            db.Client.delete({
+              TableName: db.Table,
+              Key: {
+                [db.Channel.Connections.Key]: `${db.Channel.Prefix}${db.parseEntityId(
+                  subscription[db.Channel.Primary.Key]
+                )}`,
+                [db.Channel.Connections.Range]: `${db.Connection.Prefix}${ConnectionId}`,
+              },
+            }).promise()
+          );
+
+          await Promise.all(unsubscribes);
         }
+      });
 
-        console.log(`[wsClient][send] Connection: ${connection}`);
-        console.log(`[wsClient][send] Payload: \n${JSON.stringify(payload, null, 2)}`);
-
-        // client is an instance of AWS.ApiGatewayManagementApi
-        await this.client
-            .postToConnection({ConnectionId, Data: JSON.stringify(payload)})
-            .promise()
-            .catch(async err => {
-                console.log(JSON.stringify(err));
-
-                if (err.statusCode === 410) {
-                    // unsub all channels connection was in
-                    const subscriptions = await db.fetchConnectionSubscriptions(ConnectionId);
-
-                    console.log(`[wsClient][send][postToConnection] Found stale connection, deleting ${ConnectionId}:`);
-                    console.log('[wsClient][send][postToConnection] Unsubscribe from channels:');
-                    console.log(JSON.stringify(subscriptions, null, 2));
-
-                    const unsubscribes = subscriptions.map(async subscription =>
-                        db.Client.delete({
-                            TableName: db.Table,
-                            Key: {
-                                [db.Channel.Connections.Key]: `${db.Channel.Prefix}${db.parseEntityId(subscription[db.Channel.Primary.Key])}`,
-                                [db.Channel.Connections.Range]: `${db.Connection.Prefix}${ConnectionId}`
-                            }
-                        }).promise()
-                    );
-
-                    await Promise.all(unsubscribes);
-                }
-            });
-
-        return true;
-    }
+    return true;
+  }
 }
 
 module.exports = {
-    Client
+  Client,
 };
